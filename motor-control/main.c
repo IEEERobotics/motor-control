6d003ff1eec5ee20678ebc8859739fc8540f85c2
Ethan LaMaster <ealamast@ncsu.edu>
2012-10-05 19:36:22 -0400

    Added port assignments to comment header

--- motor-control/main.c
+++ motor-control/main.c
@@ -7,6 +7,44 @@
  *
  * Does magic.
  *
+ * @section Ports
+ * Port A (Header J2):
+ * Pin 0: Unassigned
+ * Pin 1: Unassigned
+ * Pin 2: Unassigned
+ * Pin 3: Unassigned
+ * Pin 4: Unassigned
+ * Pin 5: Unassigned
+ * Pin 6: Unassigned
+ * Pin 7: Unassigned
+ *
+ * Port C (Header J4):
+ * Pin 0: Motor A0
+ * Pin 1: Motor A1
+ * Pin 2: Motor B0
+ * Pin 3: Motor B1
+ * Pin 4: Encoder 0
+ * Pin 5: Encoder 2
+ * Pin 6: Encoder 1
+ * Pin 7: Encoder 3
+ *
+ * Port D (Header J3):
+ * Pin 0: Unassigned
+ * Pin 1: Unassigned
+ * Pin 2: UART Receive (from CV board)
+ * Pin 3: UART Transmit (to CV board)
+ * Pin 4: Unassigned
+ * Pin 5: Unassigned
+ *
+ * Port F (Header J1):
+ * Pin 0: Motor D0
+ * Pin 1: Motor D1
+ * Pin 2: Motor C0
+ * Pin 3: Motor C1
+ * Pin 4: Encoder 4
+ * Pin 5: Encoder 6
+ * Pin 6: Encoder 5
+ * Pin 7: Encoder 7
  */
 
 #include <avr/io.h>
