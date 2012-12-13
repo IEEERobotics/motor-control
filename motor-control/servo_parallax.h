2706c475805bf840b5a5ae460903164726f226e2
Ethan LaMaster <ealamast@ncsu.edu>
2012-12-13 16:02:35 -0500

    Added servo_parallax.c

---
+++ motor-control/servo_parallax.h
@@ -0,0 +1,14 @@
+/*
+ * servo_parallax.h
+ *
+ *  Created on: Dec 8, 2012
+ *      Author: eal
+ */
+
+#ifndef SERVO_PARALLAX_H_
+#define SERVO_PARALLAX_H_
+
+void init_servo_parallax();
+int servo_uart_putchar(char c, FILE *f);
+
+#endif /* SERVO_PARALLAX_H_ */
