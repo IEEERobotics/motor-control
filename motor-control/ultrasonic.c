6eae431b962a213d939adf1ad5a72e5eb60fb39f
Ethan LaMaster <ealamast@ncsu.edu>
2012-12-21 16:16:44 -0500

    Finished ultrasonic code
    Not yet tested

--- motor-control/ultrasonic.c
+++ motor-control/ultrasonic.c
@@ -5,7 +5,52 @@
  *      Author: eal
  */
 
+#include <avr/io.h>
+#include <util/atomic.h>
 #include "ultrasonic.h"
+
+volatile ultrasonic_t usensors[ULTRASONIC_NUM_SENSORS];
+volatile unsigned char current_sensor = ULTRASONIC_LEFT;
+
+void init_ultrasonic()
+{
+	init_ultrasonic_struct(&usensors[ULTRASONIC_LEFT],
+						   &ULTRASONIC_LEFT_PORT,
+						   ULTRASONIC_LEFT_TRIG,
+						   ULTRASONIC_LEFT_ECHO,
+						   ULTRASONIC_LEFT_CHMUX);
+	init_ultrasonic_struct(&usensors[ULTRASONIC_FRONT],
+						   &ULTRASONIC_FRONT_PORT,
+						   ULTRASONIC_FRONT_TRIG,
+						   ULTRASONIC_FRONT_ECHO,
+						   ULTRASONIC_FRONT_CHMUX);
+	init_ultrasonic_struct(&usensors[ULTRASONIC_BOTTOM],
+						   &ULTRASONIC_BOTTOM_PORT,
+						   ULTRASONIC_BOTTOM_TRIG,
+						   ULTRASONIC_BOTTOM_ECHO,
+						   ULTRASONIC_BOTTOM_CHMUX);
+	init_ultrasonic_struct(&usensors[ULTRASONIC_RIGHT],
+						   &ULTRASONIC_RIGHT_PORT,
+						   ULTRASONIC_RIGHT_TRIG,
+						   ULTRASONIC_RIGHT_ECHO,
+						   ULTRASONIC_RIGHT_CHMUX);
+	init_ultrasonic_struct(&usensors[ULTRASONIC_BACK],
+						   &ULTRASONIC_BACK_PORT,
+						   ULTRASONIC_BACK_TRIG,
+						   ULTRASONIC_BACK_ECHO,
+						   ULTRASONIC_BACK_CHMUX);
+
+	/* Initialize ultrasonic timer */
+	ULTRASONIC_TIMER.CTRLA = TC_CLKSEL_DIV64_gc;
+	ULTRASONIC_TIMER.CTRLB = TC0_CCAEN_bm | TC_WGMODE_NORMAL_gc;
+
+	/* Enable medium priority interrupts */
+	PMIC.CTRL |= PMIC_MEDLVLEN_bm;
+	sei();
+
+	/* Trigger first sensor measurement */
+	ping_ultrasonic(&usensors[current_sensor]);
+}
 
 
 /**
@@ -17,16 +62,21 @@
  * @param echo_bm "Echo" pin bitmask
  * @param echo_chmux EVSYS_CHMUX_t corresponding to the echo pin
  */
-void init_ultrasonic(ultrasonic_t *u,
-					 PORT_t *port,
-					 uint8_t trig_bm,
-					 uint8_t echo_bm,
-					 EVSYS_CHMUX_t echo_chmux)
+void init_ultrasonic_struct(volatile ultrasonic_t *u,
+					 	 	PORT_t *port,
+					 	 	uint8_t trig_bm,
+					 	 	uint8_t echo_bm,
+					 	 	EVSYS_CHMUX_t echo_chmux)
 {
+	port->OUTSET = trig_bm;		// Set trigger high (falling edge triggered)
+	port->DIRSET = trig_bm;		// Set trigger as output
+	port->DIRCLR = echo_bm;		// Set echo as input
+
 	u->port = port;
 	u->trig_bm = trig_bm;
 	u->echo_bm = echo_bm;
 	u->echo_chmux = echo_chmux;
+	u->distance = -1;
 }
 
 
@@ -36,23 +86,62 @@ void init_ultrasonic(ultrasonic_t *u,
  * @param u Pointer to ultrasonic_t struct
  * @return Distance in millimeters
  */
-int ping_ultrasonic(ultrasonic_t *u)
+void ping_ultrasonic(volatile ultrasonic_t *u)
 {
-	int time;
+	/* Set up ultrasonic timer for pulse width capture */
+	ULTRASONIC_CHMUX = u->echo_chmux;							// Connect event channel 4 to echo pin
+	ULTRASONIC_TIMER.PER = 0xffff;
+	ULTRASONIC_TIMER.CTRLD = TC_EVACT_PW_gc | ULTRASONIC_EVSEL;	// Connect event channel 4 to timer
+	ULTRASONIC_TIMER.INTCTRLA = TC_OVFINTLVL_OFF_gc;
+	ULTRASONIC_TIMER.INTCTRLB = TC_CCAINTLVL_MED_gc;
 
-	u->port->OUTCLR = u->trig_bm;
-	u->port->DIRSET = u->trig_bm;
-	u->port->DIRCLR = u->echo_bm;
+	u->port->OUTCLR = u->trig_bm;								// Fallig edge triggers sensor measurement
+}
 
-	ULTRASONIC_CHMUX = u->echo_chmux;
-	ULTRASONIC_TIMER.CTRLA = TC_CLKSEL_DIV1_gc;
-	ULTRASONIC_TIMER.CTRLB = TC1_CCAEN_bm;
-	ULTRASONIC_TIMER.CTRLD = TC_EVACT_PW_gc | ULTRASONIC_EVSEL;
 
-	u->port->OUTSET = u->trig_bm;
-	while(ULTRASONIC_TIMER.INTFLAGS & TC1_CCAIF_bm);
-	u->port->OUTCLR = u->trig_bm;
+/**
+ * Returns the last measured distance of an ultrasonic sensor.
+ *
+ * @param u Pointer to ultrasonic_t struct
+ * @return Distance (units still undecided), or -1 if no valid distance has been measured.
+ */
+int get_ultrasonic_distance(ultrasonic_id_t index)
+{
+	int distance;
 
-	time = ULTRASONIC_TIMER.CCA;
-	return time;
+	/* This ensures that the memory access is atomic, i.e. not interrupted, as it takes multiple
+	 * clock cycles to complete.
+	 */
+	ATOMIC_BLOCK(ATOMIC_FORCEON)
+	{
+		distance = usensors[index].distance;
+	}
+
+	return distance;
+}
+
+
+/**
+ * This interrupt is triggered when the duration of the echo pulse has been measured. The timer is
+ * then reconfigured to generate a delay before the next measurement, to allow the echo to dissipate.
+ */
+ISR(ULTRASONIC_TIMER_VECT)
+{
+	usensors[current_sensor].distance = ULTRASONIC_TIMER.CCA;	// Convert to proper units
+	current_sensor = (current_sensor+1) % ULTRASONIC_NUM_SENSORS;
+
+	/* Set up ultrasonic timer to generate delay */
+	ULTRASONIC_TIMER.PER = ULTRASONIC_TIMER_OVF_PER;
+	ULTRASONIC_TIMER.CTRLD = TC_EVACT_OFF_gc | TC_EVSEL_OFF_gc;
+	ULTRASONIC_TIMER.INTCTRLA = TC_OVFINTLVL_MED_gc;
+	ULTRASONIC_TIMER.INTCTRLB = TC_CCAINTLVL_OFF_gc;
+}
+
+
+/**
+ * This interrupt triggers the next sensor measurement.
+ */
+ISR(ULTRASONIC_TIMER_OVF_VECT)
+{
+	ping_ultrasonic(&usensors[current_sensor]);
 }
--- motor-control/ultrasonic.h
+++ motor-control/ultrasonic.h
@@ -9,16 +9,72 @@
 #define ULTRASONIC_H_
 
 #include <avr/io.h>
+#include "clock.h"
 
-#define ULTRASONIC_TIMER	TCE1
-#define ULTRASONIC_CHMUX	EVSYS_CH7MUX
-#define ULTRASONIC_EVSEL	TC_EVSEL_CH7_gc
+#define ULTRASONIC_TIMER			TCE0
+#define ULTRASONIC_TIMER_VECT		TCE0_CCA_vect
+#define ULTRASONIC_TIMER_OVF_VECT	TCE0_OVF_vect
+#define ULTRASONIC_TIMER_OVF_PER	5000						// 10 ms
+#define ULTRASONIC_CHMUX			EVSYS_CH4MUX
+#define ULTRASONIC_EVSEL			TC_EVSEL_CH4_gc
+#define ULTRASONIC_NUM_SENSORS		5
+
+//#define ULTRASONIC_LEFT_INDEX		0
+#define ULTRASONIC_LEFT_PORT		PORTA
+#define ULTRASONIC_LEFT_TRIG		PIN0_bm
+#define ULTRASONIC_LEFT_ECHO		PIN1_bm
+#define ULTRASONIC_LEFT_CHMUX		EVSYS_CHMUX_PORTA_PIN1_gc
+
+//#define ULTRASONIC_FRONT_INDEX		1
+#define ULTRASONIC_FRONT_PORT		PORTA
+#define ULTRASONIC_FRONT_TRIG		PIN2_bm
+#define ULTRASONIC_FRONT_ECHO		PIN3_bm
+#define ULTRASONIC_FRONT_CHMUX		EVSYS_CHMUX_PORTA_PIN3_gc
+
+//#define ULTRASONIC_BOTTOM_INDEX		2
+#define ULTRASONIC_BOTTOM_PORT		PORTC	// !!!
+#define ULTRASONIC_BOTTOM_TRIG		PIN4_bm
+#define ULTRASONIC_BOTTOM_ECHO		PIN5_bm
+#define ULTRASONIC_BOTTOM_CHMUX		EVSYS_CHMUX_PORTC_PIN5_gc
+
+//#define ULTRASONIC_RIGHT_INDEX		3
+#define ULTRASONIC_RIGHT_PORT		PORTA
+#define ULTRASONIC_RIGHT_TRIG		PIN4_bm
+#define ULTRASONIC_RIGHT_ECHO		PIN5_bm
+#define ULTRASONIC_RIGHT_CHMUX		EVSYS_CHMUX_PORTA_PIN5_gc
+
+//#define ULTRASONIC_BACK_INDEX		4
+#define ULTRASONIC_BACK_PORT		PORTA
+#define ULTRASONIC_BACK_TRIG		PIN6_bm
+#define ULTRASONIC_BACK_ECHO		PIN7_bm
+#define ULTRASONIC_BACK_CHMUX		EVSYS_CHMUX_PORTA_PIN7_gc
+
 
 typedef struct ultrasonic {
 	PORT_t *port;
 	uint8_t trig_bm;
 	uint8_t echo_bm;
 	EVSYS_CHMUX_t echo_chmux;
+	int distance;
 } ultrasonic_t;
+
+/* Constants correspond to indices in the usensors array */
+typedef enum ultrasonic_id {
+	ULTRASONIC_LEFT 	= 0,
+	ULTRASONIC_FRONT 	= 1,
+	ULTRASONIC_BOTTOM 	= 2,
+	ULTRASONIC_RIGHT 	= 3,
+	ULTRASONIC_BACK 	= 4
+} ultrasonic_id_t;
+
+void init_ultrasonic();
+void init_ultrasonic_struct(volatile ultrasonic_t *u,
+					 	 	PORT_t *port,
+					 	 	uint8_t trig_bm,
+					 	 	uint8_t echo_bm,
+					 	 	EVSYS_CHMUX_t echo_chmux);
+void ping_ultrasonic(volatile ultrasonic_t *u);
+int get_ultrasonic_distance(ultrasonic_id_t index)
+
 
 #endif /* ULTRASONIC_H_ */
