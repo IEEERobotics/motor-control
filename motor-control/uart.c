5dc811f4a8326f938b9fcd6d8623aeca484a464a
Ethan LaMaster <ealamast@ncsu.edu>
2013-01-21 18:39:51 -0500

    Rewrote uart.c

--- motor-control/uart.c
+++ motor-control/uart.c
@@ -14,66 +14,67 @@
 #include "buffer.h"
 #include "uart.h"
 
-#define UART USARTC0
-#define UART_DREINTLVL	USART_DREINTLVL_MED_gc	// Data Register Empty interrupt priority
-#define USE_DOS_NEWLINES						// Use CRLF line endings
-#define BUFFER_SIZE		128						// UART read and write buffer size
-//#define UART_ECHO_ON					// Echo received characters back to the terminal
+uart_t debug_uart, pandaboard_uart, servo_uart;
 
 
-/**
- * Serial read and write buffers
- */
-buffer_t read_buffer, write_buffer;
-volatile uint8_t read_buffer_data[BUFFER_SIZE], write_buffer_data[BUFFER_SIZE];
-
-
-/**
- * These are the FILE streams that become stdin, stdout, and stderr.
- */
-FILE uart_in, uart_out;
-
-
-/**
- * Initializes the UART. stdio.h won't work until this function is called.
- */
-void init_uart()
+void init_uart(uart_t *u, USART_t *usart, uint16_t bsel, int8_t bscale)
 {
-	// Initialize buffers
-	buffer_init(&read_buffer, read_buffer_data, BUFFER_SIZE);
-	buffer_init(&write_buffer, write_buffer_data, BUFFER_SIZE);
+	u->usart = usart;
 
-	// Set up port pins
-	PORTC.DIRSET = PIN3_bm;
-	PORTC.DIRCLR = PIN2_bm;
+	// Initialize buffers
+	buffer_init(&(u->read_buffer), u->read_buffer_data, UART_BUFFER_SIZE);
+	buffer_init(&(u->write_buffer), u->write_buffer_data, UART_BUFFER_SIZE);
 
 	// Set interrupt priority levels
-	UART.CTRLA = USART_RXCINTLVL_MED_gc
-			   | USART_DREINTLVL_OFF_gc;
+	usart->CTRLA = USART_RXCINTLVL_MED_gc
+			   	 | USART_DREINTLVL_OFF_gc;
 
 	// Enable transmit and receive on the UART
-	UART.CTRLB = USART_RXEN_bm
-			   | USART_TXEN_bm;
+	usart->CTRLB = USART_RXEN_bm
+			   	 | USART_TXEN_bm;
 
 	// Asynchronous mode, 8 bits, no parity
-	UART.CTRLC = USART_CMODE_ASYNCHRONOUS_gc
-			   | USART_PMODE_DISABLED_gc
-			   | USART_CHSIZE_8BIT_gc;
+	usart->CTRLC = USART_CMODE_ASYNCHRONOUS_gc
+			   	 | USART_PMODE_DISABLED_gc
+			   	 | USART_CHSIZE_8BIT_gc;
 
-	// BSEL=2094, BSCALE=-7
-	// Baudrate is 115200 at 32 MHz system clock
-	UART.BAUDCTRLA = 0x2e;
-	UART.BAUDCTRLB = 0x98;
+	usart->BAUDCTRLA = bsel & 0xff;
+	usart->BAUDCTRLB = ((bsel >> 8) & 0x0f) | ((bscale << 4) & 0xf0);
+
+	// Attach the uart_t struct to the file streams. This is so that uart_putchar and uart_getchar
+	// know which buffer to use.
 
 	// Attach the uart_getchar and uart_putchar functions to the UART_IN and UART_OUT
 	// file streams
-	fdev_setup_stream(&uart_in, NULL, uart_getchar, _FDEV_SETUP_READ);
-	fdev_setup_stream(&uart_out, uart_putchar, NULL, _FDEV_SETUP_WRITE);
+	fdev_setup_stream(&(u->f_in), NULL, uart_getchar, _FDEV_SETUP_READ);
+	fdev_setup_stream(&(u->f_out), uart_putchar, NULL, _FDEV_SETUP_WRITE);
+	fdev_set_udata(&(u->f_in), (void *)u);
+	fdev_set_udata(&(u->f_out), (void *)u);
+}
+
+
+//#define NEW_INIT
+/**
+ * Initializes the UART. stdio.h won't work until this function is called.
+ */
+void init_uarts()
+{
+	// Set up port pins
+	PORTC.DIRSET = PIN3_bm;		// debug_uart
+	PORTC.DIRCLR = PIN2_bm;
+	PORTC.DIRSET = PIN7_bm;		// servo_uart
+	PORTC.DIRCLR = PIN6_bm;
+	PORTE.DIRSET = PIN3_bm;		// pandaboard_uart
+	PORTE.DIRCLR = PIN2_bm;
+
+	init_uart(&debug_uart, &DEBUG_USART, 2094, -7);				// 115200 baud at 32 MHz clock
+	init_uart(&pandaboard_uart, &PANDABOARD_USART, 2094, -7);
+	init_uart(&servo_uart, &SERVO_USART, 3329, -2);				// 2400 baud at 32 MHz clock
 
 	// Connect stdin, stdout, and stderr to the UART
-	stdin = &uart_in;
-	stdout = &uart_out;
-	stderr = &uart_out;
+	stdin = &(debug_uart.f_in);
+	stdout = &(debug_uart.f_out);
+	stderr = &(debug_uart.f_out);
 
 	// Enable medium-priority interrupts
 	PMIC.CTRL |= PMIC_MEDLVLEN_bm;
@@ -93,16 +94,18 @@ void init_uart()
  */
 int uart_putchar(char c, FILE *f)
 {
+	uart_t *u = (uart_t *)fdev_get_udata(f);
+
 #ifdef USE_DOS_NEWLINES
 	if(c == '\n')
-		uart_putchar('\r', stdout);
+		uart_putchar('\r', f);
 #endif
 
-	buffer_put(&write_buffer, c);
+	while(! buffer_put(&(u->write_buffer), c));
 
 	// Enable Data Register Empty interrupt. This will be disabled once all of the data
 	// in write_buffer has been sent.
-	UART.CTRLA = (UART.CTRLA & ~USART_DREINTLVL_gm) | UART_DREINTLVL;
+	u->usart->CTRLA = (u->usart->CTRLA & ~USART_DREINTLVL_gm) | UART_DREINTLVL;
 
 	return c;
 }
@@ -119,12 +122,14 @@ int uart_putchar(char c, FILE *f)
  */
 int uart_getchar(FILE *f)
 {
+	uart_t *u = (uart_t *)fdev_get_udata(f);
 	uint8_t c;
 
-	while(! buffer_get(&read_buffer, &c));	// Block while read_buffer is empty
+	while(! buffer_get(&(u->read_buffer), &c));	// Block while read_buffer is empty
 #ifdef UART_ECHO_ON
 	uart_putchar(c);
 #endif
+
 	return (int) c;
 }
 
@@ -136,14 +141,14 @@ int uart_getchar(FILE *f)
  * been enabled in uart_putchar(). This interrupt is disabled as soon as write_buffer
  * becomes empty (or else it would be called continuously).
  */
-ISR(USARTC0_DRE_vect)
+inline void dre_interrupt_handler(uart_t *u)
 {
 	uint8_t c;
 
-	if(buffer_get(&write_buffer, &c))
-		UART.DATA = c;
+	if(buffer_get(&(u->write_buffer), &c))
+		u->usart->DATA = c;
 	else
-		UART.CTRLA = (UART.CTRLA & ~USART_DREINTLVL_gm) | USART_DREINTLVL_OFF_gc;
+		u->usart->CTRLA = (u->usart->CTRLA & ~USART_DREINTLVL_gm) | USART_DREINTLVL_OFF_gc;
 }
 
 
@@ -152,7 +157,41 @@ ISR(USARTC0_DRE_vect)
  *
  * This ISR is called whenever the UART has received a byte.
  */
-ISR(USARTC0_RXC_vect)
+inline void rxc_interrupt_handler(uart_t *u)
 {
-	buffer_put(&read_buffer, UART.DATA);
+	buffer_put(&(u->read_buffer), u->usart->DATA);
+}
+
+
+ISR(DEBUG_USART_DRE_VECT)
+{
+	dre_interrupt_handler(&debug_uart);
+}
+
+
+ISR(DEBUG_USART_RXC_VECT)
+{
+	rxc_interrupt_handler(&debug_uart);
+}
+
+
+ISR(PANDABOARD_USART_DRE_VECT)
+{
+	dre_interrupt_handler(&pandaboard_uart);
+}
+
+
+ISR(PANDABOARD_USART_RXC_VECT)
+{
+	rxc_interrupt_handler(&pandaboard_uart);
+}
+
+ISR(SERVO_USART_DRE_VECT)
+{
+	dre_interrupt_handler(&servo_uart);
+}
+
+ISR(SERVO_USART_RXC_VECT)
+{
+	rxc_interrupt_handler(&servo_uart);
 }
--- motor-control/uart.h
+++ motor-control/uart.h
@@ -12,11 +12,37 @@
 #define SERIAL_STDIO_H_
 
 #include <stdio.h>
+#include "buffer.h"
 
+#define UART_BUFFER_SIZE	128					// UART read and write buffer size
+#define UART_DREINTLVL	USART_DREINTLVL_MED_gc	// Data Register Empty interrupt priority
+#define UART_USE_DOS_NEWLINES					// Use CRLF line endings
+//#define UART_ECHO_ON							// Echo received characters back to the terminal
 
-void init_uart();
+#define DEBUG_USART					USARTC0
+#define DEBUG_USART_DRE_VECT		USARTC0_DRE_vect
+#define DEBUG_USART_RXC_VECT		USARTC0_RXC_vect
+
+#define PANDABOARD_USART			USARTE0
+#define PANDABOARD_USART_DRE_VECT	USARTE0_DRE_vect
+#define PANDABOARD_USART_RXC_VECT	USARTE0_RXC_vect
+
+#define SERVO_USART					USARTC1
+#define SERVO_USART_DRE_VECT		USARTC1_DRE_vect
+#define SERVO_USART_RXC_VECT		USARTC1_RXC_vect
+
+typedef struct uart {
+	USART_t *usart;
+	FILE f_in, f_out;
+	buffer_t read_buffer, write_buffer;
+	volatile uint8_t read_buffer_data[UART_BUFFER_SIZE], write_buffer_data[UART_BUFFER_SIZE];
+} uart_t;
+
+void init_uart(uart_t *u, USART_t *usart, uint16_t bsel, int8_t bscale);
+void init_uarts();
 int uart_putchar(char c, FILE *f);
 int uart_getchar(FILE *f);
 
+extern uart_t debug_uart, pandaboard_uart, servo_uart;
 
 #endif /* SERIAL_STDIO_H_ */
